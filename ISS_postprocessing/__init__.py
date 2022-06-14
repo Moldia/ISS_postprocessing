197cf5b2ed5be179546413d8c27e2fe8539d3412
Christoffer Mattsson Langseth <christoffer.langseth@scilifelab.se>
2022-06-14 14:32:28 +0200

    adding stardist segmentation and map_of_clusters

--- ISS_postprocessing/__init__.py
+++ ISS_postprocessing/__init__.py
@@ -11,9 +11,11 @@ from ISS_postprocessing.annotated_objects import (get_object_info,
                                           create_ann_tiles, 
                                           spatial_neighborhood, 
                                           color_cells_gene_expression, 
-                                          plot_all_clusters
+                                          plot_all_clusters, 
+                                          map_of_clusters
                                           ) 
-from ISS_postprocessing.segmentation import (cell_pose_segemenation_to_coo, 
+from ISS_postprocessing.segmentation import (stardist_segmentation,
+                                        cell_pose_segemenation_to_coo, 
                                         hex_to_rgb, 
                                         plot_segmentation_mask_colored,
                                         segment_tile
